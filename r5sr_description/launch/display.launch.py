e98a2aa023a3b6e467dabb994804740f2c4c7d08
robohase <robohase01@gmail.com>
2023-01-05 22:56:09 +0900

    moveit2用のパッケージを作成（未動作）
    https://zenn.dev/tasada038/articles/940ef193a4a28a
    これ通りにxacroのファイルを置いて、diplay.launchでジョイントを動かせるようになった。moveit2はまだ動かせてない

--- r5sr_description/launch/display.launch.py
+++ r5sr_description/launch/display.launch.py
@@ -1,61 +1,56 @@
-from ament_index_python.packages import get_package_share_path
-
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition, UnlessCondition
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+
 
 def generate_launch_description():
-    r5sr_description_path = get_package_share_path('r5sr_description')
-    default_model_path = r5sr_description_path / 'urdf/r5sr_arm.urdf'
-    default_rviz_config_path = r5sr_description_path / 'rviz/urdf.rviz'
 
-    gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
-                                    description='Flag to enable joint_state_publisher_gui')
-    model_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_path),
-                                      description='Absolute path to robot urdf file')
-    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
-                                     description='Absolute path to rviz config file')
+    packages_name = "r5sr_description"
+    xacro_file_name = "r5sr.xacro"
+    rviz_file_name = "urdf.rviz"
 
-    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
-                                       value_type=str)
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare(packages_name), "urdf", xacro_file_name]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
 
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_description}]
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare(packages_name), "rviz", rviz_file_name]
     )
 
-    # Depending on gui parameter, either launch joint_state_publisher or joint_state_publisher_gui
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        condition=UnlessCondition(LaunchConfiguration('gui'))
+    robot_state_pub_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="both",
+        parameters=[robot_description],
     )
-
-    joint_state_publisher_gui_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        condition=IfCondition(LaunchConfiguration('gui'))
+    joint_state_pub_gui_node = Node(
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        output="screen",
     )
-
     rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
     )
 
-    return LaunchDescription([
-        gui_arg,
-        model_arg,
-        rviz_arg,
-        joint_state_publisher_node,
-        joint_state_publisher_gui_node,
-        robot_state_publisher_node,
-        rviz_node
-    ])
+    nodes = [
+        rviz_node,
+        robot_state_pub_node,
+        joint_state_pub_gui_node,
+    ]
+
+    return LaunchDescription(nodes)
