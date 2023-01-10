7b4e53a16ec026311caedbe29f2e5914bb9d1aeb
robohase <robohase01@gmail.com>
2023-01-10 16:39:38 +0900

    fix servo teleop data

--- r5sr_moveit_config/launch/r5sr_servo_teleop.launch.py
+++ r5sr_moveit_config/launch/r5sr_servo_teleop.launch.py
@@ -39,7 +39,7 @@ def generate_launch_description():
     moveit_config_folder = "r5sr_moveit_config"
     xacro_file = "urdf/r5sr.xacro"
     srdf_file = "config/r5sr.srdf"
-    rviz_file = "rviz/run_move_group.rviz"
+    rviz_file = "rviz/r5sr_servo_teleop.rviz"
 
     # planning_context
     robot_description_config = xacro.process_file(
