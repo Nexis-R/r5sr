00b1e2a7ba6dd4825d1081dd3f2998291feff3fd
Nexis-R <nexisr2016@gmail.com>
2023-01-17 16:33:58 +0900

    id 型番 合わせた

--- r5sr_manipulator_control/include/move_with_jointstate.hpp
+++ r5sr_manipulator_control/include/move_with_jointstate.hpp
@@ -9,20 +9,29 @@
 
 namespace r5sr_manipulator_control {
 enum Model {
-  MX64,
+  H54,
+  H42,
+  XH430,
+
 };
 //{model, {torque enable, goal position, pulse per rev}}
 const static std::map<Model, std::tuple<uint16_t, uint16_t, uint16_t>>
-    model_addr_map{{MX64, {64, 116, 4096}}};
+    model_addr_map{{H54, {562, 596, 501'923}},
+                   {H42, {562, 596, 303'750}},
+                   {XH430, {64, 116, 4'096}}};
 
 //{model, {torque enable, goal position, pulse per rev}}
 const static std::map<std::string, std::tuple<Model, uint8_t>>
-    jointname_id_model_map{
-        {"body0_joint_body", {MX64, 2}}, {"body1_joint", {MX64, 3}},
-        {"body2_joint", {MX64, 4}},      {"body3_joint", {MX64, 5}},
-        {"body4_joint", {MX64, 6}},      {"body5_joint", {MX64, 7}},
-        {"body6_joint", {MX64, 8}},      {"vision_arm_body1_joint", {MX64, 9}},
-    };
+    jointname_id_model_map{{"body0_joint_yaw", {H54, 2}},
+                           {"body1_joint", {H54, 3}},
+                           {"body2_joint", {H54, 4}},
+                           {"body3_joint", {H42, 5}},
+                           {"body4_joint", {H42, 9}},
+                           {"body5_joint", {H42, 6}},
+                           {"body6_joint", {H42, 7}},
+                           {"vision_arm_body1_joint", {H42, 9}},
+                           {"vision_arm_body2_joint", {XH430, 10}},
+                           {"vision_arm_body3_joint", {XH430, 11}}};
 
 class MoveWithJointState : public rclcpp::Node {
  public:
