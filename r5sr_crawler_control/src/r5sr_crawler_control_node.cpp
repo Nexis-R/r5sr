369fe20745f4c30ffbc53ac177429e5e25b9a89b
robohase <robohase01@gmail.com>
2023-04-05 11:40:28 +0900

    パッケージ新規お引越し

--- r5sr_crawler_control/src/r5sr_crawler_control_node.cpp
+++
@@ -1,8 +0,0 @@
-#include "r5sr_crawler_control/r5sr_crawler_control.hpp"
-
-int main(int argc, char** argv) {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<crawler_control::Crawler_Control>());
-  rclcpp::shutdown();
-  return 0;
-}
